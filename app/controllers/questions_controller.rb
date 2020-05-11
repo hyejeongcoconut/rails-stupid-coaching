9ea1c4c02dcda25e2a5702a57edf45c272f59e34
Hyejeong Kwon <hyejeong206@gmail.com>
2020-05-11 15:14:20 +0900

    finish

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @question = params[:question]
+    @reply= ""
+    if @question == "I am going to work"
+      @reply = "Great"
+    elsif @question.include?("?")
+      @reply = "Silly question, get dressed and go to work!"
+    else
+      @reply = "I don't care, get dressed and go to work!"
+    end
+  end
+end
